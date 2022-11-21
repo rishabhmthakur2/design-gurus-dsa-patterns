df34da3fddd5ecfff4d233b0bcfc544ec27eebcd
rishabhmthakur2 <rishabhmthakur05@gmail.com>
2022-11-21 09:17:32 +0000

    Sliding Window - Permutation in a string

--- Sliding Window/Permutation in a String.py
+++ Sliding Window/Permutation in a String.py
@@ -30,12 +30,79 @@ Output: true
 Explanation: The string contains "acb" which is a permutation of the given pattern.
 """
 
+# Unoptimized method
+
 # We will use a sliding window to go through the entire array in one pass
-# We will maintain dictionaries of character count in pattern to check if that exists in the string or not
+# We first create a Counter to keep a count of all character occurences for the pattern string
+# We then set the window length to the length of the pattern string
+# For each window, we check if the counter for that window matches the counter for the pattern
 
 from collections import Counter
 
 
-def checkPatternInString(string, pattern):
-    pass
-print(checkPatternInString("123", "abc"))
+def checkPatternPermutationInString(string, pattern):
+    windowStart = 0
+    windowEnd = len(pattern) - 1
+    patternCounter = dict(Counter(pattern))
+    while windowEnd < len(string):
+        currentString = string[windowStart:windowEnd + 1]
+        currentCounter = dict(Counter(currentString))
+        if currentCounter == patternCounter:
+            return True
+        windowStart += 1
+        windowEnd += 1
+    return False
+print(checkPatternPermutationInString("oidbcaf", "abc"))
+
+# Optimized Method
+
+def permutationInAString(s1, s2):
+    if s1 == s2:
+            return True
+        
+    if s1 == "" or len(s1) == 0:
+        return True
+    
+    if len(s1) > 0 and len(s2) == 0:
+        return False
+    
+    start = 0
+    end = 0
+
+    s1Counter = {}
+    for char in s1:
+        if char not in s1Counter:
+            s1Counter[char] = 0
+        s1Counter[char] += 1
+    
+    windowCounter = {}
+    while end < len(s2):
+        char = s2[end]
+        if char not in s1Counter:
+            windowCounter = {}
+            end += 1
+            start = end
+            continue
+
+        if char in windowCounter:
+            if windowCounter[char] + 1 > s1Counter[char]:
+                if start == end:
+                    end += 1
+                    start = end
+                    windowCounter = {}
+                else:
+                    windowCounter[s2[start]] -= 1
+                    start += 1
+                continue
+        
+        if char not in windowCounter:
+            windowCounter[char] = 0
+        
+        windowCounter[char] += 1
+
+        end += 1
+
+        if windowCounter == s1Counter:
+            return True
+        
+    return False
